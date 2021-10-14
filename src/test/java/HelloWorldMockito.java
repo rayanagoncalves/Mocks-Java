f0a1f1e4c2b8ab0d076fac60122354ca4fce8432
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2021-10-13 22:22:37 -0300

    Initial commit

---
+++ src/test/java/HelloWorldMockito.java
@@ -0,0 +1,16 @@
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Leilao;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class HelloWorldMockito {
+    @Test
+    void Hello() {
+        LeilaoDao mock = Mockito.mock(LeilaoDao.class);
+        List<Leilao> todos = mock.buscarTodos();
+        Assert.assertTrue(todos.isEmpty());
+    }
+}
